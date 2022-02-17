28ef243e72682ca22e0611714933b5d9c0c2bbfa
Johann Gomez <johanngomez02@gmail.com>
2022-02-17 15:02:54 -0500

    :hammer:/:wrench: [CREATE - MODIFY] modify list all pokemons, create card pokemon

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const pokemonDataContainer = document.getElementById("list_data_container");
+const cardContainer = document.getElementsByClassName("front-front");
+let audio = document.getElementById("pokeaudio");
+
+const playAudio = () => {
+  audio.play();
+};
 
 const getAllPokemons = async () => {
   try {
@@ -14,22 +20,51 @@ const getAllPokemons = async () => {
 
 getAllPokemons();
 
-const handleGetId = (element) => {
-  console.log(element);
-};
-
-const printDataPokemonsName = (data) => {
-  let pokemonesContainer = [];
+const printDataPokemonsName = async (data) => {
+  let pokemonesContainer = "";
 
   for (const element of data) {
-    // pokemonesContainer += `
-    // <h1 id=${element.url} onclick="handleGetId(${element})" >${element.name}</h1>
-
-    // `;
-    const h1 = document.createElement("h1", {});
-
-    pokemonesContainer;
+    pokemonesContainer += `
+     <h1 class="list_item" id=${element.name} >${element.name}</h1>
+     `;
   }
 
   pokemonDataContainer.innerHTML = pokemonesContainer;
+  let select = "";
+  const listItem = document.querySelectorAll(".list_item");
+
+  listItem.forEach((item) => {
+    item.addEventListener("click", () => {
+      select = item;
+      item.classList.add("selected");
+      playAudio();
+      listItem.forEach((item) => {
+        if (select !== item) {
+          item.classList.remove("selected");
+        }
+      });
+
+      getPokemonDetails(item.textContent);
+    });
+  });
 };
+
+const getPokemonDetails = async (pokeName) => {
+  try {
+    const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokeName}`);
+
+    const response = await data.json();
+    console.log(response);
+    console.log(response.sprites.other.dream_world.front_default);
+    printCardContainer(response.sprites.other.dream_world.front_default);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const printCardContainer = (data) => {
+  cardContainer[0].style.background = `url(${data}) no-repeat center center/cover white`;
+  console.log(cardContainer);
+};
+
+//Commit
